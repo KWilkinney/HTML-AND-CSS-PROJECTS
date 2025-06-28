0660382c72e14aaabead63858a478c95af75446d
Kyle Wilkinson <54187843+KWilkinney@users.noreply.github.com>
2025-06-28 00:46:55 -0600

    Fixed bug
    Updated element typo to allow script to run.

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
 function My_First_Function() {
 	var String = "Kiss me, I'm Irish!";
 	var result = String.fontcolor("blue");
-	document.getElementByID("Irish").innerHTML =
+	document.getElementById("Irish").innerHTML =
 	result;
 	}
